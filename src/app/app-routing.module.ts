ea30b7c6805b5385d2cf88c7ae83a6185249ab5a
113493_Grecco_Daniel <89363559+Danielazo2021@users.noreply.github.com>
2023-07-11 22:23:28 -0300

    html ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
+import { CreateEmpleadosComponent } from './components/create-empleados/create-empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'list-empleados', pathMatch: 'full'
+  },
+{
+   path: 'list-empleados', component: ListEmpleadosComponent
+},
+{ 
+  path: 'create-empleado', component: CreateEmpleadosComponent
+},
+{
+  path: '**', redirectTo: 'list-empleados', pathMatch: 'full'
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
